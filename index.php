41bd6c07bcd0698ed919b44fb49908daec21ce45
IJust17 <Feedback1414@yandex.ru>
2024-02-13 14:43:22 +0300

    lllaaa

--- index.php
+++ index.php
@@ -3,11 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
 <body>
+    <header>
+        <h1>
+            <?php
+                echo "<center> Hello World!";
+            ?>
+        </h1>
+    </header>
+    <main>
         <?php
-            echo "i hate niggers";
+            echo "Hello World!";
         ?>
+    </main>
+    <footer>
+        <?php
+            echo "Keel Knee Gears";
+        ?>
+    </footer>
 </body>
 </html>
