03e486dfc6485cc38e2cdcd491c1da7de6ed7e34
IJust17 <Feedback1414@yandex.ru>
2024-02-13 14:52:48 +0300

    laba2

--- index.php
+++ index.php
@@ -8,15 +8,33 @@
 </head>
 <body>
     <header>
-        <h1>
-            <?php
-                echo "<center> Hello World!";
-            ?>
-        </h1>
     </header>
     <main>
-        <?php
-            echo "Hello World!";
+    <?php
+    $equation = "4 * X = 36";
+    $tokens = explode(" ", $equation);
+    $first = $tokens[0];
+    $x = $tokens[2];
+    $actual_x = NAN;
+    $res = $tokens[4];
+    switch ($tokens[1]) {
+        case "+":
+            $actual_x = $res - $first;
+            break;
+        case "-":
+            $actual_x = $first - $res;
+            break;
+        case "/":
+            $actual_x = $first / $res;
+            break;
+        case "*":
+            $actual_x = $res / $first;
+            break;
+        case "**":
+            $actual_x = log($res, $first);
+            break;
+    }
+    echo "<h1 style='text-align: center;'>$actual_x</h1>"
         ?>
     </main>
     <footer>
