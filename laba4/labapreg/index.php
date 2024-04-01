2cd4b81400784910b27efd1a11c4a33fc9078303
IJust17 <Feedback1414@yandex.ru>
2024-04-01 21:03:07 +0300

    333333

--- laba4/labapreg/index.php
+++ laba4/labapreg/index.php
@@ -1,3 +1,6 @@
-<?php>
-echo preg_replace('#A3B#', '!', 'a3b A3B');
+<?php
+ echo preg_replace('#aaa(?=b)#', '!', 'aaab'); # echo preg_replace('#aaa(?<!b)#', '!', 'aaab aaab');
+ preg_match_all('#ab{4,}a#', 'aa aba abba abbba abbbba abbbbba', $matches); print_r($matches[0]);
+ preg_match_all('#ab[be]a#', 'aba aca aea abba adca abea', $matches); print_r($matches[0]);
+ echo preg_replace('#(\w)\1#', '!', 'aae xxz 33a');
 ?>
